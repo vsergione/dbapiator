1bb5860ce78e40902fffa3e88d92ac1cb09355ce
Sergiu Voicu <voicu.sergiu@gmail.com>
2019-05-14 13:02:35 +0200

    add config template

---
+++ application/config/config.tpl.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config["allApisDir"] = "/var/www/domains/apiator/apiator_data/___apis";
+//$config['base_url'] = 'https://dbapi.apiator/';
+$config['base_domain'] = ".api.apiator";
+$config['bulk_update_limit'] = 3;
+
+$config['inbound_relationships_page_size'] = 10;
+
+// default number of records/page
+$config['default_page_limit'] = 10;
+
+$config['index_page'] = '';
+$config['uri_protocol']	= 'REQUEST_URI'; // [REQUEST_URI | QUERY_STRING | PATH_INFO]
+$config['url_suffix'] = '';
+$config['language']	= 'english';
+$config['charset'] = 'UTF-8';
+$config['enable_hooks'] = FALSE;
+$config['subclass_prefix'] = 'MY_';
+$config['composer_autoload'] = FALSE;
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-,?';
+$config['allow_get_array'] = TRUE;
+$config['enable_query_strings'] = FALSE;
+$config['controller_trigger'] = 'c';
+$config['function_trigger'] = 'm';
+$config['directory_trigger'] = 'd';
+$config['log_threshold'] = 4; // 0-4
+$config['log_path'] = ''; // defaults to application/logs
+$config['log_file_extension'] = '.txt';
+$config['log_file_permissions'] = 0644;
+$config['log_date_format'] = 'Y-m-d H:i:s';
+$config['error_views_path'] = '';
+$config['cache_path'] = ''; // defaults to application/cache
+$config['cache_query_string'] = FALSE;
+$config['encryption_key'] = '';
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
+$config['cookie_prefix']	= '';
+$config['cookie_domain']	= '';
+$config['cookie_path']		= '/';
+$config['cookie_secure']	= FALSE;
+$config['cookie_httponly'] 	= FALSE;
+$config['standardize_newlines'] = FALSE;
+$config['global_xss_filtering'] = FALSE;
+$config['csrf_protection'] = FALSE;
+$config['csrf_token_name'] = 'csrf_test_name';
+$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_expire'] = 7200;
+$config['csrf_regenerate'] = TRUE;
+$config['csrf_exclude_uris'] = array();
+$config['compress_output'] = FALSE;
+$config['time_reference'] = 'local';
+$config['rewrite_short_tags'] = FALSE;
+$config['proxy_ips'] = '';
