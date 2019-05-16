40c5ac80c788d44d38ea27d2c942178e0de632ca
Sergiu Voicu <voicu.sergiu@gmail.com>
2019-05-16 14:17:38 +0200

    add config template

--- application/config/apiator.php
+++ application/config/apiator.php
@@ -2,22 +2,6 @@
 /**
  * Created by PhpStorm.
  * User: vsergiu
- * Date: 6/19/18
- * Time: 11:51 AM
+ * Date: 5/16/19
+ * Time: 2:08 PM
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config["default_result_set_limit"] = 10;
-$config["force_ssl"] = true;
-$config["apisConfigRootDir"] = BASEPATH."../apiator_data";
-$config["configStorageType"] = "file";
-$config["configStorageSettings"] = [
-    "path" => BASEPATH."../apiator_data",
-    "fileExt" => "new.json",
-    "fields"=>[
-        "conn"=>"connection",
-        "struct"=>"structure",
-        "settings"=>"settings"
-    ]
-];
-$config["adminBaseUrl"] = "/proteus/admin";
