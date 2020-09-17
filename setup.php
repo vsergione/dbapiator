ae41e3eb385f0b0e11710cc02ba92e6c0ebd35b9
Sergiu Voicu <sergiu.voicu@softaccel.net>
2020-09-17 13:00:05 +0300

    some fixes for setup script

--- setup.php
+++ setup.php
@@ -6,6 +6,7 @@ $apiatorCfgTpl = __DIR__ . "/application/config/apiator.single.php.tpl";
 $apiatorCfgDst = __DIR__ . "/application/config/apiator.php";
 $ciConfigTpl = __DIR__ . "/application/config/config.php.tpl";
 $ciConfigDst = __DIR__."/application/config/config.php";
+$db_engines = ["mysqli"];
 
 
 if(count($argv)===1) {
@@ -13,9 +14,9 @@ if(count($argv)===1) {
     echo "Description: Command line DB Apiator administration utility\n\n";
     echo "Commands:\n";
     echo "    install [/project_name/db_engine/db_host/db_user/db_pass/db_name]: Installs DbAPI on server by creating config files and\n";
-    echo "    setupdb /project_name/db_engine/db_host/db_user/db_pass/db_name: Creates a REST API for a given database\n\n";
+    echo "    newproject /project_name/db_engine/db_host/db_user/db_pass/db_name: Creates a REST API for a given database\n\n";
     echo "    regen: Regenerates an existing API\n\n";
-    echo "Setupdb Options:\n";
+    echo "newproject Options:\n";
     echo "-c /project_name/db_engine/db_host/db_user/db_pass/db_name\n";
     echo "All configuration parameters are provided directly in the command line\n";
     echo "   - project_name alfanumeric string with no spaces. Used to uniquely identify the database within the current installation\n";
@@ -39,8 +40,8 @@ switch($cmd) {
     case "install":
         install($apisConfigDir,$argv);
         break;
-    case "setupdb":
-        setupdb($apisConfigDir,$argv);
+    case "newproject":
+        newproject($apisConfigDir,$argv);
         break;
     case "regen":
         regen($apisConfigDir,array_shift($argv));
@@ -49,15 +50,8 @@ switch($cmd) {
         die("invalid command\n\n");
 }
 
-/**
- * - check app path
- * - read existing config
- */
-function install($configDir,$argv) {
-    global $apiatorCfgTpl,$apiatorCfgDst,$ciConfigTpl,$ciConfigDst;
-
-    echo "DbAPI install wizard. Please fill requested info.\n\n";
-
+function handle_apis_config_dir_creation($configDir)
+{
     // Projects base dir
     $readConfigDir = readline("Configurations base directory [".$configDir."]: ");
     if($readConfigDir!=="")
@@ -70,17 +64,23 @@ function install($configDir,$argv) {
             if(!mkdir($configDir))
                 die("Could not create $configDir");
     }
+}
 
-
+function handle_main_config_file_creation($configDir)
+{
+    global $apiatorCfgTpl,$apiatorCfgDst;
     $apiatorCfg = file_get_contents($apiatorCfgTpl);
     if($apiatorCfg===false)
         die("DB Apiator config template not found");
+
     $apiatorCfg = str_replace("%%conn_dir_path%%",$configDir,$apiatorCfg);
     if(file_put_contents($apiatorCfgDst,$apiatorCfg)===false)
         die("Could not save $apiatorCfgDst config file");
+}
 
-
-
+function handle_ci_config_file()
+{
+    global $ciConfigTpl,$ciConfigDst;
     $ciConfig = file_get_contents($ciConfigTpl);
     if($ciConfig===false)
         die("CI config template not found");
@@ -91,6 +91,7 @@ function install($configDir,$argv) {
         $baseUrl = $cfg["base_url"];
         $prompt = "Base URL for DBAPI [".$cfg["base_url"]."]";
     }
+
     $readBaseUrl = readline($prompt);
     $readBaseUrl = (empty($readBaseUrl) && $baseUrl)?$baseUrl:$readBaseUrl;
 
@@ -102,11 +103,29 @@ function install($configDir,$argv) {
 
     if(file_put_contents($ciConfigDst,$ciConfig)===false)
         die("Could not save $ciConfigDst config file");
+}
+
+/**
+ * - check app path
+ * - read existing config
+ */
+function install($configDir,$argv) {
+    echo "DbAPI install wizard. Please fill requested info.\n\n";
+
+    // Step 1: check if exists and if not create projects base directory
+    handle_apis_config_dir_creation($configDir);
+
+    // Step 2: generate pnpbase configuration file
+    handle_main_config_file_creation($configDir);
+
+    // Step 3: generate CodeIgniter configuration file
+    handle_ci_config_file();
 
     echo "\n";
+
     $setupConfirm = readline("Do you want to configure a project ([Y]/N)?");
     if(in_array(strtoupper($setupConfirm),["","Y"]))
-        setupdb($configDir,$argv);
+        newproject($configDir,$argv);
 }
 
 function regen($configDir,$projName)
@@ -122,31 +141,36 @@ function regen($configDir,$projName)
     ];
     if(is_file($projPath."/parse_helper.php"))
         $args[] = $projPath."/parse_helper.php";
-    setupdb($configDir,$args);
+    newproject($configDir,$args);
 }
 
 /**
  * @param $params
- * @return array
+ * @return array[$project_name,$command]
  */
 function get_parameters($params)
 {
-    $pattern = "/\/([a-z0-9]+)\/(mysqli)\/([0-9a-z\.\-\_]+(\:[0-9]{2,5})?)\/([a-z][a-z0-9\.\_\-]+)\/(.*)\/([a-z][a-z0-9\.\_\-]+)/";
+    global $db_engines;
     if(isset($params[0])) {
+        $pattern = "/\/([a-z0-9]+)\/(mysqli)\/([0-9a-z\.\-\_]+(\:[0-9]{2,5})?)\/([a-z][a-z0-9\.\_\-]+)\/(.*)\/([a-z][a-z0-9\.\_\-]+)/";
         if(!preg_match($pattern,$params[0],$matches))
             die("Invalid configuration parameters. See help.\n\n");
-        $projName = $matches[1];
+
         $cmd = sprintf("dbapi/ConfigGen/cli/%s/%s/%s/%s/%s",$matches[2],$matches[3],$matches[5],$matches[6],$matches[7]);
-        return [$projName,$cmd];
+
+        return [$matches[1],$cmd];
     }
 
     // interactive mode
-    $read = readline("Project name (alphanumeric): ");
-    $projName = $read===""?"localhost":$read;
+    $cnt = 0;
+    do {
+        $projName = readline(($cnt?"Project name cannot be empty. ":"")."Project name (alphanumeric): ");
+        $cnt++;
+    } while(!$projName);
 
     $read = readline("Database engine [mysqli]: ");
     $engine = $read===""?"mysqli":$read;
-    if($engine!=="mysqli")
+    if(!in_array($engine,$db_engines))
         die("\nError: Database engine '$engine' not supported\n");
 
     $read = readline("Database host (eg: localhost:3306) [localhost]: ");
@@ -161,49 +185,61 @@ function get_parameters($params)
     return [$projName,$cmd];
 }
 
-/**
- * @param $configDir
- * @param $cli_args
- */
-function setupdb($configDir,$cli_args) {
-    global $apiatorCfgTpl,$apiatorCfgDst,$ciConfigTpl,$ciConfigDst;
-
-    list($projName,$cmd) = get_parameters($cli_args);
-
-
-    if(!$cmd)
-        die("No options provided. Nothing to do.\n\n");
-
+function handle_project_directory($configDir,$projName)
+{
     $projPath = $configDir."/".$projName;
 
-    if(!is_dir($projPath) && !mkdir($projPath))
+    if(is_dir($projPath)) {
+        $read = readline("Project $projName already exists. Do you want to overwrite? ([Y]/N):");
+        if (!in_array(strtolower($read), ["y", ""]))
+            die("Setup canceled\n");
+    }
+
+    if(!mkdir($projPath))
         die("Could not create project directory '$projPath'");
 
-    $read = readline("Project $projName already exists. Do you want to overwrite? ([Y]/N):");
-    if(!in_array(strtolower($read),["y",""]))
-        die("Setup canceled\n");
+    return $projPath;
+}
 
-
+function exec_gen($cli_args,$cmd,$projPath)
+{
     $cmd .= "/".urlencode(realpath($projPath));
     if(isset($cli_args[1]) && is_file($cli_args[1])) {
         $cmd .= "/".urlencode(realpath($cli_args[1]));
     }
-    elseif (is_file(getcwd()."/helper.php")) {
-        $cmd .= "/".urlencode(realpath(getcwd()."/helper.php"));
-    }
+//    elseif (is_file(getcwd()."/helper.php")) {
+//        $cmd .= "/".urlencode(realpath(getcwd()."/helper.php"));
+//    }
+
     $cmd = "php ".__DIR__."/public/index.php $cmd";
 //    echo  $cmd;
     exec( $cmd,$output,$ret);
-//    print_r($output);
+    return $ret;
+}
+/**
+ * @param $configDir
+ * @param $cli_args
+ */
+function newproject($configDir, $cli_args) {
+
+    // step 1: get project parameters
+    list($projName,$cmd) = get_parameters($cli_args);
+
+    // step 2: handle project directory
+    $projPath = handle_project_directory($configDir,$projName);
+
+    // step 3: generated project files
+    $ret = exec_gen($cli_args,$cmd,$projPath);
+
+    // print_r($output);
     if($ret===0) {
         $cfg = include __DIR__."/application/config/config.php";
         echo "\nAPI successfully created and available at:\n\t {$cfg["base_url"]}/v2/$projName\n\n";
-        echo "Config files available at: \n\t$projPath\n\n";
+        echo "Project files available at: \n\t$projPath\n\n";
         return;
     }
 
-//    recursive_delete(realpath($projPath));
-
+    // recursive_delete(realpath($projPath));
 }
 
 
